5807e87b14c3232b3a862b514ab792ba0c2d408a
Sergey Antonyuk <san@outsi.de>
2023-10-03 14:18:02 +1000

    Post list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
-  Hello world
+  <div v-for="post of posts" :key="post.id">
+    <Post
+        v-bind:post="post"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import Post from "@/components/PostItem.vue";
 
+export default {
+  components: {Post},
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  methods: {
+    fetchPosts() {
+      fetch('https://jsonplaceholder.typicode.com/posts?_page=2&_limit=2')
+          .then(async res => {
+            this.posts = await res.json()
+          })
+          .catch((reason) => console.log(reason))
+    },
+  },
+  created() {
+    this.fetchPosts();
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  height: 100%;
+}
+
+body {
+  min-height: 100%;
+  background: #37393A;
+}
 
 </style>
---
+++ src/components/PostItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="post">
+    <div class="post__title">
+      {{ post.title }}
+    </div>
+    <div class="post__body">
+      {{ post.body }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object,
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 10px 15px;
+  margin: 5px 15px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  background: #77B6EA;
+  color: black;
+  border-radius: 10px;
+}
+
+.post__title {
+  font-weight: bold;
+}
+</style>
